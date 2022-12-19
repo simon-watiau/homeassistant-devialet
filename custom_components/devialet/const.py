0939895eca346718460439d58e374f69b0e7eec0
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2022-12-19 14:13:49 +0100

    Update const.py

--- custom_components/devialet/const.py
+++ custom_components/devialet/const.py
@@ -8,30 +8,9 @@ DEFAULT_SCAN_INTERVAL = 5
 MANUFACTURER: Final = "Devialet"
 LOGGER = logging.getLogger(__package__)
 
-NORMAL_INPUTS = {
-    "Airplay": "airplay2",
-    "Bluetooth": "bluetooth",
-    "Digital left": "digital_left",
-    "Digital right": "digital_right",
-    "Line": "line",
-    "Online": "upnp",
-    "Optical": "optical",
-    "Optical left": "optical_left",
-    "Optical right": "optical_right",
-    "Optical jack": "opticaljack",
-    "Phono": "phono",
-    "Raat": "raat",
-    "Spotify Connect": "spotifyconnect",
-}
-
 SOUND_MODES = {
     "Custom": "custom",
     "Flat": "flat",
     "Night mode": "night mode",
     "Voice": "voice",
-}
-
-SPEAKER_POSITIONS = {
-    "FrontLeft": "left",
-    "FrontRight": "right",
 }
